022c6e58159acbc9f2970407290102ed4ed3303f
Emir <Emir@DESKTOP-SR1EIQS>
2023-01-30 04:31:46 +0300

    Proje dosyası ekle.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using DockerApp.Models;
+using System.Security.Cryptography;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.VisualBasic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DockerApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+       public UserController(UserDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult>Register(UserRegisterRequest request)
+        {
+            if (_context.Users.Any(u => u.Email == request.Email))
+            {
+                return BadRequest("Bu email kullanılmakta");
+            }
+            CreatePasswordHash(request.Password,out byte[] passwordHash,out byte[] passwordSalt);
+            var user = new User
+            {
+                Email = request.Email,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                VerificationToken = CreateRandomToken()
+            };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok("Başarıyla Kayıt Olundu.");
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginRequest request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+            if(user == null)
+            {
+                return BadRequest("Kullanıcı Bulunamadı");
+            }
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Yanlış şifre!");
+            }
+
+            return Ok($"Hoşgeldiniz,{user.Email}! :)");
+        }
+        [HttpPost("verify")]
+        public async Task<IActionResult> Verify(string token)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.VerificationToken == token);
+            if (user == null)
+            {
+                return BadRequest("Invalid token.");
+            }
+
+            user.VerifiedAt = DateTime.Now;
+            await _context.SaveChangesAsync();
+
+            return Ok("Kullanıcı Doğrulandı :)");
+        }
+        private void CreatePasswordHash(string password, out byte[] passwordHash,out byte[] passwordSalt)
+        {
+            using (var hmac=new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        private bool VerifyPasswordHash(string password,  byte[] passwordHash,  byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+               var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+        private string CreateRandomToken()
+        {
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("my top secret key"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(expires: DateTime.Now.AddDays(1), signingCredentials: creds);
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+
+        }
+    }
+}
